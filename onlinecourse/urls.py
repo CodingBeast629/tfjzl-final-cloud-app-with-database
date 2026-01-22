ed09340ae5b38b9e0bf485ab7583756c136f19a7
Taksh <takshpendap@gmail.com>
2026-01-21 20:45:53 -0500

    Edited views.py and urls.py

--- onlinecourse/urls.py
+++ onlinecourse/urls.py
@@ -12,13 +12,22 @@ urlpatterns = [
     path('registration/', views.registration_request, name='registration'),
     path('login/', views.login_request, name='login'),
     path('logout/', views.logout_request, name='logout'),
+
     # ex: /onlinecourse/5/
     path('<int:pk>/', views.CourseDetailView.as_view(), name='course_details'),
-    # ex: /enroll/5/
+
+    # ex: /onlinecourse/5/enroll/
     path('<int:course_id>/enroll/', views.enroll, name='enroll'),
 
-    # <HINT> Create a route for submit view
+    # ✅ Task 5: submit exam
+    path('<int:course_id>/submit/', views.submit, name='submit'),
 
-    # <HINT> Create a route for show_exam_result view
+    # ✅ Task 5: show exam result
+    path(
+        'course/<int:course_id>/submission/<int:submission_id>/result/',
+        views.show_exam_result,
+        name='exam_result'
+    ),
 
- ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
